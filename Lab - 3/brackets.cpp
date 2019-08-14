a73851259942a96531bb4f5550ab255622249cbb
FalakChhikara <45658281+FalakChhikara@users.noreply.github.com>
2019-08-14 15:11:16 +0530

    Add files via upload

---
+++ Lab - 3/brackets.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isBalanced(string x)
+{
+	stack<char>s;
+	long long int l=x.size();
+	if(l%2!=0){return false;}
+	
+	for(long long int i=0;i<l;i++)
+	{
+		if(x[i]=='(' || x[i]=='{' || x[i]=='[' || x[i]=='<') 
+		{
+			s.push(x[i]);
+			
+		}
+		else if(x[i]==')' || x[i]=='}' || x[i]==']' || x[i]=='>'){
+			if(s.empty())
+			{
+			    return false;
+			}
+			else
+			{
+				if(x[i]==')')
+				{
+					if(s.top()!='(') return false;
+					else s.pop();
+				}
+				
+				else if(x[i]=='}')
+				{
+					if(s.top()!='{') return false;
+					else s.pop();
+				}
+				
+				else if(x[i]==']')
+				{
+					if(s.top()!='[') return false;
+					else s.pop();
+				}
+				
+				else if(x[i]=='>')
+				{
+					if(s.top()!='<') return false;
+					else s.pop();
+				}
+			}
+		}
+		else if(x[i]=='|')
+		{	if(!s.empty())
+			{
+				if(s.top()!='|') 
+    			{
+    				s.push(x[i]);
+    			}
+    			else 
+    			{
+					s.pop();
+    			}
+			}
+			else
+			{
+				s.push(x[i]);
+			}
+		
+		}
+		else
+		{
+		    return false;
+		}
+	
+	}
+	if(s.empty()) return true;
+	else return false;
+}
+int main()
+{
+	long long int t;
+	cin>>t;
+	start:
+	while(t--)
+	{
+		string x;
+		cin>>x;
+		if(isBalanced(x)) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}
+}
+
